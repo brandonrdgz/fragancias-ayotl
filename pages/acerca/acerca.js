29228b76647529e6f80507af444caf53fcf76533
CarlosPena <carlos_pena@outlook.de>
2024-11-04 12:39:08 -0600

    C-A19 | Intentnado meter React

--- pages/acerca/acerca.js
+++ pages/acerca/acerca.js
@@ -1,13 +1,14 @@
 const hiddenElements = document.querySelectorAll(".hidden");
+// Configura el observador
 const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    } else {
-      entry.target.classList.remove("show");
-    }
-  });
-});
-
+    entries.forEach((entry) => {
+        console.log("Observando elemento:", entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
+        }
+    });
+}, { threshold: 0.1 }); // Puedes ajustar el threshold para sensibilidad de aparición
+// Observa cada elemento oculto
 hiddenElements.forEach((el) => observer.observe(el));
