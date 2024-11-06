2472d64e831e895eb23366ca4e7d2ecd9ef01467
Arturo Peralta <arturo.pesilva@gmail.com>
2024-11-05 21:50:10 -0600

    borrando archivos

--- pages/acerca/acerca.js
+++ pages/acerca/acerca.js
@@ -1,30 +1,15 @@
-<<<<<<< HEAD
-export function logic_main()
-{
+export function logic_main() {
   const hiddenElements = document.querySelectorAll(".hidden");
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-      // console.log(entry);
+      console.log(entry);
       if (entry.isIntersecting) {
         entry.target.classList.add("show");
       } else {
         entry.target.classList.remove("show");
       }
     });
-=======
-if (window.acercaObserver) return;
-
-const hiddenElements = document.querySelectorAll(".hidden");
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    } else {
-      entry.target.classList.remove("show");
-    }
->>>>>>> 7a90659 (argregano formulario para nuevos productos)
   });
-  
+
   hiddenElements.forEach((el) => observer.observe(el));
 }
