0e10f78f2095f077d1d516fde8fdfee1f7c93f12
Arturo Peralta <arturo.pesilva@gmail.com>
2024-11-05 18:33:33 -0600

    argregano formulario para nuevos productos

--- pages/acerca/acerca.js
+++ pages/acerca/acerca.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 export function logic_main()
 {
   const hiddenElements = document.querySelectorAll(".hidden");
@@ -10,6 +11,19 @@ export function logic_main()
         entry.target.classList.remove("show");
       }
     });
+=======
+if (window.acercaObserver) return;
+
+const hiddenElements = document.querySelectorAll(".hidden");
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+>>>>>>> 7a90659 (argregano formulario para nuevos productos)
   });
   
   hiddenElements.forEach((el) => observer.observe(el));
