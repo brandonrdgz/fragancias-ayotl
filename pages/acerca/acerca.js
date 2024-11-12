1e2234892bb35091c8d673052ff6746e864da529
CarlosPena <carlos_pena@outlook.de>
2024-11-11 22:09:49 -0600

    C-B30 | Primera gran parte de la refactorización completada

--- pages/acerca/acerca.js
+++ pages/acerca/acerca.js
@@ -2,7 +2,7 @@ export function logic_main() {
   const hiddenElements = document.querySelectorAll(".hidden");
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-      console.log(entry);
+      // console.log(entry);
       if (entry.isIntersecting) {
         entry.target.classList.add("show");
       } else {
