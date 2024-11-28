35a76b7517c049ecfea2a6eac7de6e609b2c85a4
Arturo Peralta <arturo.pesilva@gmail.com>
2024-11-27 23:37:50 -0600

    KAN-33 agregar pagina 404

--- pages/catalogo/catalogo.jsx
+++ pages/catalogo/catalogo.jsx
@@ -1,35 +1,31 @@
 return async ({ cardProduct }) => {
   const IMG_FOLDER = "/assets/imgs/perfumes/";
-  let obtenerProductos = (await import("/js/utils/crudJSON.js"))['obtenerProductos'];
+  let obtenerProductos = (await import("/js/utils/crudJSON.js"))[
+    "obtenerProductos"
+  ];
 
   const productos = await obtenerProductos("/data/fragancias.json");
 
   let perfumes = "";
   if (productos) {
-    const divPerfumesContainer = document.querySelector(
-      "#perfumes"
-    );
+    const divPerfumesContainer = document.querySelector("#perfumes");
     // caracteristicas.tamaño
     perfumes = productos.reduce((acc, val) => {
-      
-      const {precio, nombre, img, moneda, caracteristicas, id } = val;
-      const ID= id;
-      const TITLE= nombre;
-      const TAMAÑO= caracteristicas.tamaño;
-      const IMG= IMG_FOLDER + img;
-      const PRECIO= precio;
-      const MONEDA= moneda;
-      return acc+= cardProduct(
-        {
-
-          ID,
-          TITLE,
-          TAMAÑO,
-          IMG,
-          PRECIO,
-          MONEDA
-        }
-      );
+      const { precio, nombre, img, moneda, caracteristicas, id } = val;
+      const ID = id;
+      const TITLE = nombre;
+      const TAMAÑO = caracteristicas.tamaño;
+      const IMG = IMG_FOLDER + img;
+      const PRECIO = precio;
+      const MONEDA = moneda;
+      return (acc += cardProduct({
+        ID,
+        TITLE,
+        TAMAÑO,
+        IMG,
+        PRECIO,
+        MONEDA,
+      }));
     }, "");
   }
   return (
@@ -42,4 +38,4 @@ return async ({ cardProduct }) => {
       </section>
     </>
   );
-}
+};
