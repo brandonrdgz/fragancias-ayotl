2568548ef028e7680c08e89d0e1f1d68da3c32bf
Arturo Peralta <arturo.pesilva@gmail.com>
2024-12-03 23:08:22 -0600

    agregando la pagina de producto unico

--- pages/product/product.js
+++ pages/product/product.js
@@ -1,1 +1,26 @@
+const params = new URLSearchParams(window.location.search);
+const productId = params.get("id");
 
+fetch("../../data/fragancias.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const producto = data.find((item) => item.id === productId);
+
+    if (producto) {
+      const productContainer = document.getElementById("product-container");
+      productContainer.innerHTML = `
+        <div class="product-details">
+          <img src="${producto.img}" alt="${producto.title}" />
+          <h1>${producto.title}</h1>
+          <p>${producto.description}</p>
+          <span>${producto.price}</span>
+          <button class="btn add-cart">Agregar al Carrito</button>
+        </div>
+      `;
+    } else {
+      console.error("Producto no encontrado");
+    }
+  })
+  .catch((error) =>
+    console.error("Error al cargar los datos del producto:", error),
+  );
