e324f17f333d0ad57d64bef2aa3eb39762191074
CarlosPena <carlos_pena@outlook.de>
2024-12-04 11:01:10 -0600

    FIX Githubpages

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -1,5 +1,5 @@
 return async ({ navbarElement }) => {
-  let executeAfter = (await import("../../utils/executeAfter.js"))['executeAfter'];
+  let executeAfter = (await import("../../js/utils/executeAfter.js"))['executeAfter'];
 
   function init() {
     let menu = document.querySelector("#menu-icon");
