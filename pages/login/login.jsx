4eabd308b5160caa8292b2177a83ddab429069a2
CarlosPena <carlos_pena@outlook.de>
2024-12-04 10:55:36 -0600

    FIX Githubpages

--- pages/login/login.jsx
+++ pages/login/login.jsx
@@ -1,5 +1,5 @@
 return async () => {
-  let executeAfter = (await import("../../js/utils/executeAfter.js"))['executeAfter'];
+  let executeAfter = (await import("../../utils/executeAfter.js"))['executeAfter'];
 
   function init() {
     /*=============== SHOW HIDE PASSWORD LOGIN ===============*/
